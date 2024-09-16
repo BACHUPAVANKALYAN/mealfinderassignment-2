b0570fd27ce5fd373a62b02b263c0455731b6834
BACHUPAVANKALYAN <150771394+BACHUPAVANKALYAN@users.noreply.github.com>
2024-09-16 16:06:41 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Meal Finder</h1>
+      <input type="search" />
+      <h1>Break Fast</h1>
+      <img src="https://images.app.goo.gl/NXwVvkyGrSfqjTQX6" />
+      <img src="https://images.app.goo.gl/7JGSvPQuhhoKsVTL7" />
+    </div>
+  );
+}
